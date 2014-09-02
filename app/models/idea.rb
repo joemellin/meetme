6b851b84254e4dff79fdcd7ae7bb0a7dcd4c822b
Joe Mellin <mellin.joe@gmail.com>
2014-09-01 22:43:20 -0700

    update nav

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :cover => "1600x400#", :thumb => "100x100#", :small => "200x200#", :medium => "300x300#"}
+	has_attached_file :image, :styles => { :cover => "1600x600#", :thumb => "100x100#", :small => "200x200#", :medium => "300x300#"}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
