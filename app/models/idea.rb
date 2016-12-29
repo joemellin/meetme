40ea308b0b80d9c9fc73af3f006b40483d4b5004
Joe Mellin <mellin.joe@gmail.com>
2016-12-28 21:56:21 -0800

    making it a bit easier

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,7 +4,7 @@ class Idea < ActiveRecord::Base
 
 	accepts_nested_attributes_for :iterations, :reject_if => :all_blank, :allow_destroy => true
 
-	has_attached_file :image, :styles => { :cover => "1600x350#", :thumb => "100x100#", :small => "200x200#", :medium => "300x300#"}
+	has_attached_file :image, :styles => { :cover => "1600x450#", :thumb => "100x100#", :small => "200x200#", :medium => "300x300#"}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
